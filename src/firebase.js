a10eeffad28453a5801ed1c641e7aea18bb60d94
jaehwanjung <jhnara1995@gmail.com>
2021-07-23 16:47:45 +0900

    feat : Add initial setting(connect firebase, ..)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const firebaseConfig = {
+  apiKey: "AIzaSyBJs4B0SSGcZV7pTfQaVYlA37hdqE8FAmQ",
+  authDomain: "facebook-clone-d9c0c.firebaseapp.com",
+  projectId: "facebook-clone-d9c0c",
+  storageBucket: "facebook-clone-d9c0c.appspot.com",
+  messagingSenderId: "464727061911",
+  appId: "1:464727061911:web:2a521750f34c935b6e80d2",
+  measurementId: "G-79J0YZFDX7",
+};
