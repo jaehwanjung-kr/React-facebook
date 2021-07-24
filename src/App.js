e24dbeba9c64059ccfb02b826ea8660f5ed59895
jaehwanjung <jhnara1995@gmail.com>
2021-07-24 23:08:15 +0900

    feat: Add header__left section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Lets build the facebook clone</h1>
 
-      {/* Header */}
+      <Header />
       {/* App body */}
       {/* Sidebar */}
       {/* Feed */}
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,12 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <div className="header__left"></div>
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+          alt=""
+        />
+      </div>
 
       <div className="header__middle"></div>
 
