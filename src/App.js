07cee7bed6cda4a88511d05eea62266715971c33
jaehwanjung <jhnara1995@gmail.com>
2021-07-29 21:23:14 +0900

    feat: Add 'Sponsored' Sections

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Sponsored from "./Sponsored";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
+        <Sponsored />
       </div>
     </div>
   );
---
+++ src/Sponsored.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sponsored.css";
+
+function Sponsored() {
+  return <div className="sponsored"></div>;
+}
+
+export default Sponsored;
